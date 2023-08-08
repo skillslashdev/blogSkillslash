d3ba3eb290ac254fa4bf08b024df392a6a8c5739
somnathraz <somnathkhadanga810@gmail.com>
2023-08-08 14:06:30 +0530

    blog cdn

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: "https://skillslash-cdn.s3.ap-south-1.amazonaws.com",
+  images: {
+    domains: ["skillslash-cdn.s3.ap-south-1.amazonaws.com"],
+  },
   reactStrictMode: true,
   swcMinify: true,
 };
 
 module.exports = nextConfig;
-module.exports = {
-  images: {
-    domains: ["skillslash-cdn.s3.ap-south-1.amazonaws.com"],
-  },
-};
